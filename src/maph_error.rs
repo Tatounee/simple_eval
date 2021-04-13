200600756adeec084cc4b1fa51a22398bb4299f1
Tatounee <oettam32@gmail.com>
2021-04-13 19:06:31 +0200

    Add an Error struct

---
+++ src/maph_error.rs
@@ -0,0 +1,30 @@
+use crate::ast::token::operator::Operator;
+
+#[derive(Debug, PartialEq)]
+pub struct Error {
+    pub kind: ErrorKinds,
+    pub span: Vec<usize>,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKinds, span: Vec<usize>) -> Self {
+        Self { kind, span }
+    }
+
+    #[inline]
+    pub fn add_span(&mut self, span: usize) {
+        for s in self.span.iter_mut() {
+            *s = span - *s
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ErrorKinds {
+    InvalideChar(char),
+    MultipleComma,
+    UnknowOperator(String),
+    UnclosedBrack,
+    UnexeptedBrack,
+    UnexeptedOperator(Operator)
+}

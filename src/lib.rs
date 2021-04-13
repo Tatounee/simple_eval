1caba2a03c55633f374255532d3bcd975e802b49
Tatounee <oettam32@gmail.com>
2021-04-13 19:05:59 +0200

    Remove useless test

--- src/lib.rs
+++ src/lib.rs
@@ -7,24 +7,3 @@ mod tree;
 mod operator;
 mod token;
 
-use expr::Expr;
-use tree::TreeNode;
-use operator::Operator;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    #[ignore]
-    fn parse_add() {
-        let expr: Expr = "2 + 3".parse().unwrap();
-        assert_eq!(expr, Expr::Node(
-            Box::new(TreeNode{
-                l_expr: Expr::Number(2.),
-                op: Operator::Add,
-                r_expr: Expr::Number(3.)
-            })
-        ))
-    }
-}
